c832d13a9f29c1ae9ce1d79b44485a790559e199
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-09-11 13:51:28 +0200

    add hw txt

--- ChallengeH/challengeh.js
+++ ChallengeH/challengeh.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const primaryValid = isNaN primaryPhone 
+const secondaryValid = isNaN secondaryPhone
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
